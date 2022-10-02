26650f6536a78e43e4767101f22295a8025807eb
gowriswarupkailas <contactgowri2000@gmail.com>
2022-10-02 12:50:14 +0100

    LabsK01 part2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.slf4j:slf4j-simple:2.0.1")
+    implementation("io.github.microutils:kotlin-logging:3.0.0")
 }
 
 tasks.test {
--- src/main/kotlin/org/setu/placemark/main.kt
+++ src/main/kotlin/org/setu/placemark/main.kt
@@ -1,8 +1,13 @@
 package org.setu.placemark
 
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>){
     println("LabsK01")
     println("Placemark Kotlin App Version 1.0")
+
+    logger.info { "Launching Placemark Console App"}
 }
 
