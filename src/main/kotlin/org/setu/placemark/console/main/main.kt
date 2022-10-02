9bfdfca67e124725ecde6d23cb092e2a892e7967
gowriswarupkailas <contactgowri2000@gmail.com>
2022-10-02 15:03:29 +0100

    LabsK02 part2

--- src/main/kotlin/org/setu/placemark/console/main/main.kt
+++ src/main/kotlin/org/setu/placemark/console/main/main.kt
@@ -1,12 +1,15 @@
 package org.setu.placemark.console.main
 
 import mu.KotlinLogging
+import org.setu.placemark.console.models.PlacemarkModel
 
 private val logger = KotlinLogging.logger {}
 
+var placemark = PlacemarkModel()
+
 fun main(args: Array<String>) {
     logger.info { "Launching Placemark Console App" }
-    println("Placemark Kotlin App Version 1.0")
+    println("Placemark Kotlin App Version 2.0")
 
     var input: Int
 
@@ -15,7 +18,7 @@ fun main(args: Array<String>) {
         when(input) {
             1 -> addPlacemark()
             2 -> updatePlacemark()
-            3 -> listAllPlacemarks()
+            3 -> listPlacemarks()
             -1 -> println("Exiting App")
             else -> println("Invalid Option")
         }
@@ -23,18 +26,18 @@ fun main(args: Array<String>) {
     } while (input != -1)
     logger.info { "Shutting Down Placemark Console App" }
 }
+
 fun menu() : Int {
-
     var option : Int
-    var input: String? = null
+    var input: String?
 
-    println("Main Menu")
+    println("MAIN MENU")
     println(" 1. Add Placemark")
     println(" 2. Update Placemark")
     println(" 3. List All Placemarks")
     println("-1. Exit")
     println()
-    print("Enter an integer : ")
+    print("Enter Option : ")
     input = readLine()!!
     option = if (input.toIntOrNull() != null && !input.isEmpty())
         input.toInt()
@@ -44,16 +47,27 @@ fun menu() : Int {
 }
 
 fun addPlacemark(){
-    println("You Chose Add Placemark")
-
+    println("Add Placemark")
+    println()
+    print("Enter a Title : ")
+    placemark.title = readLine()!!
+    print("Enter a Description : ")
+    placemark.description = readLine()!!
+    println("You entered [ " + placemark.title + " ] for title " +
+            "and [ " + placemark.description + " ] for description")
 }
 
-fun updatePlacemark(){
-    println("You Chose Update Placemark")
-
+fun updatePlacemark() {
+    println("Update Placemark")
+    println()
+    print("Enter a new Title for [ " + placemark.title + " ] : ")
+    placemark.title = readLine()!!
+    print("Enter a new Description for [ " + placemark.description + " ] : ")
+    placemark.description = readLine()!!
+    println("You updated [ " + placemark.title + " ] for title " +
+            "and [ " + placemark.description + " ] for description")
 }
 
-fun listAllPlacemarks(){
+fun listPlacemarks() {
     println("You Chose List All Placemarks")
-
 }
